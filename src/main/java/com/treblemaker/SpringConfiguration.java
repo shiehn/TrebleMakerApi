674b5bd35bd4980194eb22ae3e82ca8c877d7a3f
steve hiehn <stevehiehn@gmail.com>
2019-01-29 20:42:02 -0800

    update to JDK11

--- src/main/java/com/treblemaker/SpringConfiguration.java
+++ src/main/java/com/treblemaker/SpringConfiguration.java
@@ -11,12 +11,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @EnableAutoConfiguration
